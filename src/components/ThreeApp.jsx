b59971d708ea06c03411e3d864ee65a0a9a83b4e
DRT Software <tony@drt-software.com>
2023-03-23 23:31:48 +0000

    Add physics to scene with fixed floor

--- src/components/ThreeApp.jsx
+++ src/components/ThreeApp.jsx
@@ -1,16 +1,23 @@
 import { OrbitControls, Sphere } from "@react-three/drei";
 import { SCENE } from "../config/Config.js";
+import {Physics, RigidBody} from "@react-three/rapier";
 import Floor from "./Floor.jsx";
 
 const ThreeApp = () => {
 
   return (
     <>
+    <Physics>
       <ambientLight intensity={SCENE.ambientIntensity} />
       <pointLight position={SCENE.lightPosition} />
-      <Floor />
+      <RigidBody>
       <Sphere position-y={5} material-color="hotpink"/>
+      </RigidBody>
+      <RigidBody type="fixed">
+      <Floor />
+      </RigidBody>
       <OrbitControls />
+      </Physics>
     </>
   );
 };

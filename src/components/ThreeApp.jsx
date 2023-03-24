2493308742d77daa50a01080ed8ece3b3154e797
DRT Software <tony@drt-software.com>
2023-03-24 01:20:39 +0000

    Add fixed target to aim at

--- src/components/ThreeApp.jsx
+++ src/components/ThreeApp.jsx
@@ -1,5 +1,5 @@
 import {useRef} from "react";
-import { OrbitControls, Sphere } from "@react-three/drei";
+import { OrbitControls, Sphere, Box } from "@react-three/drei";
 import { SCENE } from "../config/Config.js";
 import {Physics, RigidBody} from "@react-three/rapier";
 import Floor from "./Floor.jsx";
@@ -9,7 +9,7 @@ const ThreeApp = () => {
   const sphereRef = useRef();
 
   const throwSphere = event => {
-    sphereRef.current.applyImpulse({x: 0, y:0, z:-100});
+    sphereRef.current.applyImpulse({x: 0, y:20, z:-100});
   }
 
   return (
@@ -19,6 +19,9 @@ const ThreeApp = () => {
       <pointLight position={SCENE.lightPosition} />
       <RigidBody ref={sphereRef} colliders="ball" restitution={1.5} position={[0, 1, 11]}>
         <Sphere onClick={throwSphere} material-color="hotpink"/>
+      </RigidBody>
+      <RigidBody type="fixed" position={[0, 1, -10]}>
+        <Box material-color="blue" />
       </RigidBody>
       <RigidBody type="fixed">
         <Floor />

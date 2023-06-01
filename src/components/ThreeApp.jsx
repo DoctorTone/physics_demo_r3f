c613f656cb38c1740223da49898cba6599f73691
DoctorTone <drtonesoftware@gmail.com>
2023-06-01 10:47:06 +0100

    Disable orbit controls

--- src/components/ThreeApp.jsx
+++ src/components/ThreeApp.jsx
@@ -39,7 +39,7 @@ const ThreeApp = ({ thrown }) => {
           <Floor />
         </RigidBody>
       </Physics>
-      <OrbitControls enablePan={false} enableRotate={false} />
+      {/* <OrbitControls enablePan={false} enableRotate={false} /> */}
       <Pool position={SCENE.poolPosition} />
       <Figure scale={SCENE.FIGURE_SCALE} position={SCENE.figurePosition} />
     </>
